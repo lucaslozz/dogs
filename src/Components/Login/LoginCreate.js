241d88cde97eabb8082adc81f8b7aae5de00f992
lucaslozz <lucass.mark@gmail.com>
2022-12-30 10:01:49 -0300

    criado a validação do formulario de cadastro

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import styles from '../../App.css';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
 
 const LoginCreate = () => {
-  return <div>Login Create</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  function handleSubmit(event) {
+    event.preventDefault(username);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input type="text" label="Usuário" name="usuario" {...username} />
+        <Input type="text" label="Email" name="email" {...email} />
+        <Input type="password" label="Senha" name="senha" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
